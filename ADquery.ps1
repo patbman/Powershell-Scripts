620c85a290ee24ce07c3d46849b06d3f4f491fd1
pat <patbman5@gmail.com>
2020-09-03 12:39:40 -0400

    Query AD initial upload
    Query AD for computer name and description

---
+++ ADquery.ps1
@@ -0,0 +1,5 @@
+﻿foreach($line in (Get-ADComputer -Filter * -SearchBase "OU=REPLACE,OU=REPLACE,OU=REPLACE,DC=REPLACE,DC=REPLACE" -Properties CanonicalName, Name, Description | Select -Property CanonicalName, Description)){
+    $x = ($line.CanonicalName -replace '[^\/]*\/[^\/]*\/[^\/]*\/[^\/]*\/', '').Replace("/",", ")+', ' + $line.description
+    add-content ADquery.txt $x
+    
+}
